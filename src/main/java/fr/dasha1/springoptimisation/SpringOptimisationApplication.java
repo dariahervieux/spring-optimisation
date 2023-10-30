6bc0374bb691c368b7445b5558417f90dcc395a1
Daria HERVIEUX <19156262+dariahervieux@users.noreply.github.com>
2023-10-30 23:19:33 +0100

    Removing non-needed dependencies + diactivating unnecessary autoconfiguration

--- src/main/java/fr/dasha1/springoptimisation/SpringOptimisationApplication.java
+++ src/main/java/fr/dasha1/springoptimisation/SpringOptimisationApplication.java
@@ -2,11 +2,23 @@ package fr.dasha1.springoptimisation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.availability.ApplicationAvailabilityAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
+import org.springframework.boot.autoconfigure.ssl.SslAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 /**
  * https://timeular.com/blog/best-prioritization-techniques/
  */
-@SpringBootApplication
+@SpringBootApplication (exclude = {SslAutoConfiguration.class, ApplicationAvailabilityAutoConfiguration.class,
+		ProjectInfoAutoConfiguration.class, TaskExecutionAutoConfiguration.class, TaskSchedulingAutoConfiguration.class,
+		WebSocketServletAutoConfiguration.class, SpringDataWebAutoConfiguration.class,
+		RestTemplateAutoConfiguration.class, ErrorMvcAutoConfiguration.class
+})
 public class SpringOptimisationApplication {
 
 	public static void main(String[] args) {
