a283b8e0b8165c220cfd3f51b68f29f232cf4c25
Daria HERVIEUX <19156262+dariahervieux@users.noreply.github.com>
2023-11-01 00:48:04 +0100

    Initializing Quarkus project

--- src/main/java/fr/dasha1/springoptimisation/infra/db/ToDoItemsCrudRepository.java
+++ src/main/java/fr/dasha1/springoptimisation/infra/db/ToDoItemsCrudRepository.java
@@ -4,8 +4,10 @@ import fr.dasha1.springoptimisation.domain.model.Status;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ToDoItemsCrudRepository extends CrudRepository<ToDoItemEntity, Long> {
 
-    Iterable<ToDoItemEntity> findAllByStatusIsNot(Status status);
+    List<ToDoItemEntity> findAllByStatusIsNot(Status status);
 }
