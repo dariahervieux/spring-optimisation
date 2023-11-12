92d587b592a688da5590d137bf3c3442a5dd0b3a
dhervieux <19156262+dariahervieux@users.noreply.github.com>
2023-11-13 00:05:31 +0100

    Adjusting RSS command

--- mesure/rss.sh
+++ mesure/rss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+PIDOFJAVA=$(ps -e -o pid,args |grep "java -jar" | grep build | awk '{print $1}')
 
-PIDOFJAVA=$(ps -e -o pid,args | grep java | grep spring| awk '{print $1}')
 echo "PID of java application: $PIDOFJAVA"
 echo "RSS using pmap: $(pmap -x $PIDOFJAVA)"
