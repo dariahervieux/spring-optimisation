58403e719b6bd811f77e5ad678864bc812e81f92
dhervieux <19156262+dariahervieux@users.noreply.github.com>
2023-11-13 00:45:56 +0100

    Adjusting application startup script

--- mesure/startapp.sh
+++ mesure/startapp.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # spring app
-APP=build/libs/spring-optimisation-0.0.1-SNAPSHOT.jar
-if test -f "$APP"
-then
-    echo "Starting spring app.."
-    date +"%T.%3N"  && java -jar $APP
-fi
+SAPP=build/libs/spring-optimisation-0.0.1-SNAPSHOT.jar
+
+echo "Starting spring app.."
+date +"%T.%3N"  && java -Dspring.aot.enabled=true -jar $SAPP
