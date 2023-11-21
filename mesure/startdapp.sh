48d8804e11b4a249f6b2fbfdc263d8aea29d8878
dhervieux <19156262+dariahervieux@users.noreply.github.com>
2023-11-21 23:09:27 +0100

    Changing image name

--- mesure/startdapp.sh
+++ mesure/startdapp.sh
@@ -2,5 +2,5 @@
 
 # spring app in a docker
 echo "Starting spring app.."
-date +"%T.%3N"  && sudo docker run -p 8080:8080 -v "$(pwd)"/db:/db docker.io/library/spring-optimisation:0.0.1-SNAPSHOT
+date +"%T.%3N"  && sudo docker run --rm -p 8080:8080 -v "$(pwd)"/db:/db docker.io/library/spring-optimisation:main
 
